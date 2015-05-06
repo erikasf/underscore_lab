1031d46b0d215646f2d4293a07f616743cf6a02a
erika <erika@erikasf.com>
2015-05-06 05:22:41 -0700

    added part two

--- underscore.js
+++ underscore.js
@@ -135,6 +135,38 @@ function pluck(obj, key) {
     return _.map(obj, _.property(key));
   };
   var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
-_.pluck(stooges, 'name');
+_.pluck(stooges, 'name');+##Part 2
++
++* each - iterates over an array and calls a given function with each element
++
++```
++_.each([1, 2, 3], alert);
++=> alerts each number in turn...
++```
++
++* compact - returns a new array with all provided undefined values removed
++
++```
++_.compact([1, "hello", undefined, 3, undefined]);
++=> [1, "hello", 3]
++```
++
++* map - returns a new array of values produced by running each element of an array through a given function
++
++```
++_.map([1, 2, 3], function(num){ return num * 3; });
++=> [3, 6, 9]
++
++_.map(["dogs", "before", "cats"], function(str){ return str.toUpperCase(); });
++=> ["DOGS", "BEFORE", "CATS"]
++
++```
++
++* filter - Looks through each value in the list, returning an array of all the values that pass a truth test
++
++```
++var evens = _.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
++=> [2, 4, 6]
+
 => ["moe", "larry", "curly"]
 }
